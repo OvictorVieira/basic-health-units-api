1d3dc3a9fae1a001ca9d831a7cf8f001bf97f441
Victor Hugo Vieira <victor.h.souza.vieira@gmail.com>
2020-05-03 17:11:01 -0300

    it starts to queue the worker only for the UBSs that do not exist in the database

--- app/workers/institute_creator_worker.rb
+++ app/workers/institute_creator_worker.rb
@@ -1,11 +1,10 @@
 class InstituteCreatorWorker
 
   include Sidekiq::Worker
-  include InstituteCreationPolicy
 
   sidekiq_options retry: 3, backtrace: true
 
   def perform(institute = {})
-    Institute.create!(institute) unless institute_already_exists?(institute)
+    Institute.create!(institute)
   end
 end
